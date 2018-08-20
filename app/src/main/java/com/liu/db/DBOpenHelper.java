ae60af129609fd289d6d741d0460f42ddb9c3825
1160899400 <1160899400@qq.com>
2018-08-20 19:03:16 +0800

    2018/8/20

---
+++ app/src/main/java/com/liu/db/DBOpenHelper.java
@@ -0,0 +1,29 @@
+package com.liu.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author HZLI02
+ * @date 2018/8/20
+ */
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    private final String CREATE_SQL = "CREATE TABLE test(_id INTEGER PRIMARY KEY AUTOINCREMENT,name)";
+
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
